fafda93de4ac261e4cf850445f1b9d440e51e65f
Daniela Mansilla <danielamansill29@gmail.com>
2024-11-13 20:42:13 -0300

    Entrega Final

--- api/server.js
+++ api/server.js
@@ -2,10 +2,12 @@ const express = require('express') // va a llamar a express en node_modules
 const mongoose = require('mongoose');
 const dotenv= require('dotenv').config()
 
+const url = process.env.DATABASE_URL
 const app = express()
+const PORT = process.env.PORT
 
 //const url = 'mongodb+srv://danielamansill29:zcV7oS2j5qiwXaNF@plataforma-disco.v9pgd.mongodb.net/?retryWrites=true&w=majority&appName=Plataforma-Disco';
-const url = process.env.DATABASE_URL
+
 const router = require('./routes/index.js')
 const user = require('./models/User.js')
 const album = require('./models/Album.js')
@@ -20,10 +22,11 @@ console.log(process.env)
 app.use(express.json());
 app.use(cookieParser()) // CAE EL SERVIDOR
 
+
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")))
 
-
+app.use("/health", (req, res) => res.sendStatus(200));
 
 const apiRoutes = require('./routes/index');
 
@@ -41,7 +44,7 @@ app.use('/imagenes', express.static('C:/Users/danij/OneDrive/Escritorio/Platafor
 const connectToMongo =async ()=>{
     try{
         await mongoose.connect(url)
-        app.listen(4500, ()=>{
+        app.listen(PORT, ()=>{
             console.log('Servidor escuchando en puerto 4500 y DB conectada')
         });
 
